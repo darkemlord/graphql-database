82387b26a7f89a1d56eac14de14af9c759a1bb8b
emanuel caro <darkemlord@gmail.com>
2021-12-15 16:36:12 +0900

    user model created

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./gql/typeDefs');
 const resolvers = require('./gql/resolvers');
 const mongoose = require('mongoose');
+require('dotenv').config( { path: ".env" }); //calling the ENV
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
-mongoose.connect()
+
+console.log(process.env.DB)
+mongoose.connect(process.env.DB, { useNewUrlParser: true })
 
 server.listen({ port: 5000})
   .then(({url}) => {
